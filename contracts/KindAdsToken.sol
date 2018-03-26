0208e6e9ee4e0e55a10b042a52c917ab855ab65e
Enrique Chavez <noone@tmeister.net>
2018-03-26 12:23:43 -0600

    Add CanReclaimToken function
      - Add the ability to the owner to recover trapped tokens
      - Fix #3

--- contracts/KindAdsToken.sol
+++ contracts/KindAdsToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.18;
 
 import "./zeppelin-solidity/contracts/token/StandardToken.sol";
 import "./zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./zeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
 
 
 /**
